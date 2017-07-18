048d8c4249436ae83ef17e3b57c8cfeb6ed3ac6b
James Higgins and Sowmya Dinavahi <james+sowmya@>
2017-07-18 11:50:17 -0700

    add rate limiting

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,23 +1,10 @@
 class Place < ApplicationRecord
   validates :city, :country, :presence => true
   has_many :reviews
-  #  scope :search, -> (query)
+
   scope :search_by_city, -> (city_parameter){ where("city like ?", "%#{city_parameter}%")}
   max_paginates_per 25
   paginates_per 25
 
 
-  #  scope :search, -> (city){ where('city like ?', '%#{city}%')}
-
-  #
-  #OR "country like ?" "%#{query}%"
-  # scope :search, -> (query) do
-  #     Place.all.find_all do |place|
-  #     place.city.match("#{query}") ||
-  #     place.country.match("#{query}")
-  #   end
-  # end
-  #
-
-  # scope :search_by_country, -> (country_parameter){ where("country like ?", "%#{country_parameter}%")}
 end
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'rack/throttle'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -20,6 +21,7 @@ module TravelApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.middleware.use Rack::Throttle::Interval, :min => 10.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
